e1eab54f0d1cabc96f0d574a8343f2ae6734252b
Spheniscine <spheniscine@gmail.com>
2026-06-23 15:33:40 +0800

    card model

---
+++ src/game/mod.rs
@@ -0,0 +1,17 @@
+mod card;
+pub use card::*;
+
+mod skin;
+pub use skin::*;
+
+// mod board;
+// pub use board::*;
+
+// mod game;
+// pub use game::*;
+
+// mod settings;
+// pub use settings::*;
+
+// mod utils;
+// pub use utils::*;
